9783c0e1112dd7118394c994941463a88ba708a4
Daniel Laserna Condado <condadodaniel@hotmail.com>
2017-02-28 20:37:16 +0000

    Updating AI - Building BT

--- Source/BeaconOfLife/BeaconOfLifeCharacter.cpp
+++ Source/BeaconOfLife/BeaconOfLifeCharacter.cpp
@@ -37,7 +37,7 @@ ABeaconOfLifeCharacter::ABeaconOfLifeCharacter()
 	// Create a decal in the world to show the cursor's location
 	CursorToWorld = CreateDefaultSubobject<UDecalComponent>("CursorToWorld");
 	CursorToWorld->SetupAttachment(RootComponent);
-	static ConstructorHelpers::FObjectFinder<UMaterial> DecalMaterialAsset(TEXT("Material'/Game/TopDownCPP/Blueprints/M_Cursor_Decal.M_Cursor_Decal'"));
+	static ConstructorHelpers::FObjectFinder<UMaterial> DecalMaterialAsset(TEXT("Material'/Game/Materials/M_Cursor_Decal.M_Cursor_Decal'"));
 	if (DecalMaterialAsset.Succeeded())
 	{
 		CursorToWorld->SetDecalMaterial(DecalMaterialAsset.Object);
--- Source/BeaconOfLife/BeaconOfLifeGameMode.cpp
+++ Source/BeaconOfLife/BeaconOfLifeGameMode.cpp
@@ -11,7 +11,7 @@ ABeaconOfLifeGameMode::ABeaconOfLifeGameMode()
 	PlayerControllerClass = ABeaconOfLifePlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_Player"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
